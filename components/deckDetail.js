3d97c1253bed274dcf012dd1a3c3a14ca5d2578f
Courtney Noonan <courtneynoonan@me.com>
2017-11-20 12:17:46 -0500

    adding icon and styling deck detail view

--- components/deckDetail.js
+++ components/deckDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 export default class DeckDetail extends Component {
   render() {
@@ -7,8 +8,11 @@ export default class DeckDetail extends Component {
     console.log(this.props.navigation.state.params)
     return (
       <View style={styles.container}>
-        <Text>{deck.title}</Text>
-        <Text>{deck.questions.length}</Text>
+        <View style={styles.deck}>
+          <Text style={styles.title}>{deck.title.toUpperCase()}</Text>
+          <MaterialCommunityIcons style={styles.icon} name="cards" size={30} />
+          <Text>{deck.questions.length} cards</Text>
+        </View>
       </View>
     )
   }
@@ -17,7 +21,22 @@ export default class DeckDetail extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  deck: {
     backgroundColor: '#4286f4',
     alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    height: 200,
+    borderRadius: 7,
+  },
+  icon: {
+    margin: 5,
+  },
+  title: {
+    fontSize: 20,
   },
 })
