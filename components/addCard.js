500c03ef5659564f4efe0c83d0dc0ff3ce1964ad
Courtney Noonan <courtneynoonan@me.com>
2017-11-22 18:59:56 -0500

    adding text input to add card component

--- components/addCard.js
+++ components/addCard.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
 
 export default class AddCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      question: '',
+      answer: '',
+    }
+  }
   render() {
     return (
       <View>
-        <Text>Add Card</Text>
+        <TextInput
+          style={{
+            height: 80,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginBottom: 10,
+          }}
+          value={this.state.question}
+          onChangeText={question => this.setState({ question })}
+          placeholder="Question"
+          multiline={true}
+        />
+        <TextInput
+          style={{ height: 80, borderColor: 'gray', borderWidth: 1 }}
+          value={this.state.answer}
+          onChangeText={answer => this.setState({ answer })}
+          placeholder="Answer"
+          multiline={true}
+        />
       </View>
     )
   }
