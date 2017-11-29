3f2f51744bf1c8f7dff7eeabcfb3e037285b775f
Courtney Noonan <courtneynoonan@me.com>
2017-11-29 16:59:28 -0500

    adding functionality to cycle through questions

--- components/quiz.js
+++ components/quiz.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 
 class Quiz extends Component {
   state = {
     index: 0,
+    toggleCard: 'question',
+  }
+
+  showAnswer() {
+    console.log('button pressed')
+    this.setState(state => {
+      return {
+        ...state,
+        toggleCard: 'answer',
+      }
+    })
+    console.log('made it here')
+  }
+
+  incrementIndex() {
+    this.setState(state => {
+      return {
+        ...state,
+        toggleCard: 'question',
+        index: state.index + 1,
+      }
+    })
   }
 
   render() {
@@ -12,15 +34,43 @@ class Quiz extends Component {
     const { questions } = this.props.decks[
       this.props.navigation.state.params.deckName
     ]
-    return (
-      <View>
-        <Text>Quiz</Text>
-        <Text>{questions[this.state.index].question}</Text>
-        <TouchableOpacity>
-          <Text>Show Answer</Text>
-        </TouchableOpacity>
-      </View>
-    )
+    if (this.state.index > questions.length - 1) {
+      return (
+        <View>
+          <Text>finished!</Text>
+        </View>
+      )
+    }
+    if (this.state.toggleCard === 'question') {
+      return (
+        <View style={styles.container}>
+          <View style={styles.card}>
+            <Text>{questions[this.state.index].question}</Text>
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.showAnswer()}
+          >
+            <Text>Show Answer</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+    if (this.state.toggleCard === 'answer') {
+      return (
+        <View style={styles.container}>
+          <View style={styles.card}>
+            <Text>{questions[this.state.index].answer}</Text>
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.incrementIndex()}
+          >
+            <Text>Next</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
   }
 }
 
@@ -29,5 +79,31 @@ function mapStateToProps(state) {
     decks: state,
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    backgroundColor: '#4286f4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    height: 200,
+    borderRadius: 7,
+  },
+  button: {
+    marginTop: 30,
+    borderRadius: 7,
+    width: 80,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gold',
+  },
+})
 
 export default connect(mapStateToProps)(Quiz)
